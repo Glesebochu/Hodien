d59fae74e0def20226bda0a79e5d98b630ec21b4
intann316 <intisarmohammed03@gmail.com>
2025-05-03 23:03:03 +0300

    integrate content fetching and implement persistence with Firestore

--- frontend/lib/pages/home.dart
+++ frontend/lib/pages/home.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import '../services/user_service.dart';
-import '../models/user.dart' as user_model;
+//import '../services/user_service.dart';
+//import '../models/user.dart' as user_model;
 import 'package:google_fonts/google_fonts.dart';
 import 'post_card.dart';
 import '../services/engine.dart';
@@ -42,6 +42,7 @@ class _HomeState extends State<Home> {
   void initState() {
     super.initState();
     _initializeProfile();
+    //await HumorProfile.loadFavoriteContentStack(); // Load favorite content stack
   }
 
   Future<void> _initializeProfile() async {
@@ -51,12 +52,11 @@ class _HomeState extends State<Home> {
       print('No user logged in');
       return;
     }
-
     // Initialize the profile with the userId
     setState(() {
       profile = HumorProfile(userId: user.uid);
     });
-
+    await profile.loadFavoriteContentStack();
     // Initialize the humor engine after profile is set
     engine = HumorEngine(profile: profile);
 
