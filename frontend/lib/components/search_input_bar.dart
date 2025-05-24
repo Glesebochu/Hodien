f8af00920dd9be94d3ec9cc7065097be5a6d8d7d
Yan8i <141297750+Yan8i@users.noreply.github.com>
2025-05-24 12:50:26 +0300

    fix: adjust horizontal padding in search input bar for improved layout

--- frontend/lib/components/search_input_bar.dart
+++ frontend/lib/components/search_input_bar.dart
@@ -123,7 +123,7 @@ class _SearchInputBarState extends State<SearchInputBar> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 25),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
