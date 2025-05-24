30d0df68b30c71b7247b1fafe10792f43ed8f9ce
Yan8i <141297750+Yan8i@users.noreply.github.com>
2025-05-24 12:53:54 +0300

    fix: update search input hint text for improved user engagement

--- frontend/lib/components/search_input_bar.dart
+++ frontend/lib/components/search_input_bar.dart
@@ -187,7 +187,7 @@ class _SearchInputBarState extends State<SearchInputBar> {
               }
             },
             decoration: InputDecoration(
-              hintText: 'Search',
+              hintText: 'Hey',
               hintStyle: TextStyle(
                 color: const Color.fromARGB(255, 147, 147, 147),
                 fontSize: 17.5,
